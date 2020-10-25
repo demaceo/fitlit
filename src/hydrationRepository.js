ea0ceb72ff3b5498bf8eb0764b723f0563732046
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-10-24 21:05:17 -0500

    Initial organization and exploration of files. Creation of hydrationRepository. Debugged UserRepository TDD

---
+++ src/hydrationRepository.js
@@ -0,0 +1,23 @@
+class HydrationRepository {
+  constructor () {
+    this.records = []
+  }
+  updateHydration(date, amount) {
+    this.ouncesRecord.unshift({[date]: amount});
+    if (this.ouncesRecord.length) {
+      this.ouncesAverage = Math.round((amount + (this.ouncesAverage * (this.ouncesRecord.length - 1))) / this.ouncesRecord.length);
+    } else {
+      this.ouncesAverage = amount;
+    }
+  }
+  addDailyOunces(date) {
+    return this.ouncesRecord.reduce((sum, record) => {
+      let amount = record[date];
+      if (amount) {
+        sum += amount
+      }
+      return sum
+    }, 0)
+  }
+}
+export default HydrationRepository; 
