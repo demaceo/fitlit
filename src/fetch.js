098f7b69c1f186230ca2712a6428c56cb5b029c4
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-10-26 17:14:53 -0500

    Build get fetch for sleepData

--- src/fetch.js
+++ src/fetch.js
@@ -16,4 +16,31 @@ function fetchData() {
   })
   .catch(err => console.log(err.message))
 
-  
+  let sleepData = fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/sleep/sleepData
+')
+    .then (response => response.json());
+    .then(data => {
+      return data.sleepData;
+  })
+  .catch(err => console.log(err.message));
+
+  let activityData = fetch()
+    .then(response => response.json())
+    .then(data => {
+      return data.activityData;
+  })
+  .catch(err => console.log(err.message))
+
+
+  return Promise.all({userData, hydrationData, sleepData, activityData})
+    .then(data => {
+      let rawData = {}
+      rawData.userData = data[0];
+      rawData.hydrationData = data[1];
+      rawData.sleepData = data[2];
+      rawData.activityData = data[3];
+    return rawData
+  });
+  }
+
+  export default fetchData
