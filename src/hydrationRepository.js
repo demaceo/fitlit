132c75239e778d7dd7f0d7cdea52fad441790e06
Demaceo Howard <hdemaceo@gmail.com>
2020-10-27 16:06:52 -0600

    Refactor HydrationRepository methods and properties

--- src/hydrationRepository.js
+++ src/hydrationRepository.js
@@ -1,69 +1,54 @@
 class HydrationRepository {
   constructor() {
     this.records = [];
-    this.totalAverageOunces = 0;
-    // this.dailyAverageOunces = 0;
-    this.ouncesRecord = [];
-    this.weeklyOunces = [];
+    // this.totalAverageOunces = 0;
+    // this.ouncesRecord = [];
+    // this.weeklyOunces = [];
   }
 
-  updateAverageOunces(record) {
-    this.ouncesRecord.unshift(record);
-    if (this.ouncesRecord.length) {
-      this.totalAverageOunces = Math.round((record.numOunces + (this.totalAverageOunces * (this.ouncesRecord.length - 1))) / this.ouncesRecord.length);
-    } else {
-      this.totalAverageOunces = record.numOunces;
-    }
+  // updateAverageOunces(record) {
+  //   this.ouncesRecord.unshift(record);
+  //   if (this.ouncesRecord.length) {
+  //     this.totalAverageOunces = Math.round((record.numOunces + (this.totalAverageOunces * (this.ouncesRecord.length - 1))) / this.ouncesRecord.length);
+  //   } else {
+  //     this.totalAverageOunces = record.numOunces;
+  //   }
+  // }
+  addDailyOunces(userID, date, numOunces) {
+    this.records.push({
+      userID: userID.
+      date: date,
+      numOunces: numOunces
+    })
   }
 
-  calculateAverageDailyOunces(date) {
-    let dailyOunces = 0
-    let dailyAverageOunces;
+  calculateUserAverageTotalOunces(userID) {
+    let totalOunces = 0;
     let counter = 0;
     this.records.forEach(record => {
-    if(record.date === date){
-      dailyOunces += record.numOunces;
-      counter ++
-    }
-    })
-    return dailyOunces / counter;
-  }
-  calculateAverageWeeklyOunces(week) {
-    let weeklyOunces = 0;
-    let counter = 0;
-    week.forEach(record => {
-    weeklyOunces += record.numOunces;
-    counter++
-    })
-    return weeklyOunces / counter
-  }
-
-  addDailyOunces(date) {
-    return this.ouncesRecord.reduce((sum, record) => {
-      let amount = record[date];
-      if (amount) {
-        sum += amount
+      if (record.userID === userID) {
+        totalOunces += record.numOunces;
+        counter++
       }
-      return sum
-    }, 0)
+    })
+    return totalOunces = totalOunces / counter;
   }
-  calculateUserAverageDailyOunces(userID, date){
-  let averageDailyOunces = 0;
-  let counter = 0;
-  mockHydrationRepo.forEach(user => {
-  if(user.userID === userID && user.date === date){
-    averageDailyUsers += user.numOunces
-    counter++;
+
+  calculateAverageDailyOunces(userID, date) {
+    let averageDailyOunces = 0;
+    let counter = 0;
+    this.records.forEach(record => {
+      if (record.userID === userID && record.date === date) {
+        averageDailyUsers += record.numOunces
+        counter++;
+      }
+    })
+    return averageDailyOunces = averageDailyOunces / counter;
   }
-  })
- return averageDailyOunces = averageDailyOunces / counter
-}
 
-calculateUserAverageWeeklyOunces(userID, date){
+  calculateAverageWeeklyOunces(userID, date) {
 
-}
-calculateUserAverageTotalOunces(userID){
-}
+  }
 }
 
 export default HydrationRepository;
