0639b2ae44e577fa2fe134001575c14fce80d782
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-10-27 18:23:43 -0500

    Modify subfolders from record to records

--- src/hydrationRepository.js
+++ src/hydrationRepository.js
@@ -1,27 +1,13 @@
 class HydrationRepository {
   constructor() {
     this.records = [];
-    this.ouncesAverage = 0;
-    this.ouncesRecord = [];
+
   }
   updateAverageOunces(date, amount) {
-    this.ouncesRecord.unshift({
-      [date]: amount
-    });
-    if (this.ouncesRecord.length) {
-      this.ouncesAverage = Math.round((amount + (this.ouncesAverage * (this.ouncesRecord.length - 1))) / this.ouncesRecord.length);
-    } else {
-      this.ouncesAverage = amount;
-    }
+
   }
   addDailyOunces(date) {
-    return this.ouncesRecord.reduce((sum, record) => {
-      let amount = record[date];
-      if (amount) {
-        sum += amount
-      }
-      return sum
-    }, 0)
+    
   }
 }
 
